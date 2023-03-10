8d281c5810c56b58aedebd812d88b7a3cf2b10b1
Gautamchy2002 <112376740+Gautamchy2002@users.noreply.github.com>
2023-03-11 00:07:05 +0530

    Add files via upload

---
+++ BreakEx1.java
@@ -0,0 +1,21 @@
+
+public class BreakEx1 {
+	public static void main(String[] args) {
+		for(int x=1;x<=8;x++) { 
+			System.out.println("Hello");
+			//if(x==3) {
+//			if(x>=3) {
+			if(x++>=3) {
+				break;
+			}
+			System.out.println("Rahul Chauhan");
+			
+//			if(x==3) {
+//				break;
+//			}else {
+//				break;
+//			}
+//			System.out.println("Rahul Chauhan");//error
+		}
+	}
+}
