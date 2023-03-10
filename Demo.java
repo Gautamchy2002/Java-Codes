48516a9056997604fa2cc96ebbf55d62394b8844
Gautamchy2002 <112376740+Gautamchy2002@users.noreply.github.com>
2023-03-11 01:01:21 +0530

    Add files via upload

--- Demo.java
+++ Demo.java
@@ -1,10 +1,9 @@
-package my;
 
-public class Demo {
-
-	public static void main(String[] args) {
-		System.out.println("Hello gautam");
-
-	}
-
+class Demo{
+    static public void main(String []s){
+        System.out.printf("hi INCAPP\n");
+        int a=10,b=20;
+        int r=a+b;
+        System.out.println(r); 
+    }
 }
