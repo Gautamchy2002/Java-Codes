fc6f1708ca3669b7a874294c17df559c5bdc70a2
Gautamchy2002 <112376740+Gautamchy2002@users.noreply.github.com>
2023-03-11 00:00:25 +0530

    Add files via upload

---
+++ NonParametrizedConstructorEx.java
@@ -0,0 +1,25 @@
+package NonParametrizedConstructor;
+
+class Person{
+	String name;
+	int age;
+	void show() {
+		System.out.println(name+" "+age);
+	}
+	Person(){
+		System.out.println("Hello Person");
+		name="Ram";
+		age=10;
+	}
+}
+public class NonParametrizedConstructorEx {
+	public static void main(String[] args) {
+		Person p1=new Person();
+		Person p2=new Person();
+		Person p3=new Person();
+		//p1.Person();//error
+		p1.show();
+		p2.show();
+		p3.show();
+	}
+}
